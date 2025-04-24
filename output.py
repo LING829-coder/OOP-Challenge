0cc114e79b26b4f6771f2a356365356d92a41b0b
LING829-coder <lingmukiri13@gmail.com>
2025-04-24 06:20:36 -0900

    Create output.py
    my output for the challenge

---
+++ output.py
@@ -0,0 +1,15 @@
+
+Dog played happily!
+Dog ate some food. Hunger decreased, happiness increased!
+Dog took a nap. Energy restored!
+Dog learned a new trick: sit!
+Dog learned a new trick: roll over!
+
+Dog's Status:
+Hunger: #####----- (5/10)
+Energy: #####----- (5/10)
+Happiness: ########-- (8/10)
+Dog is getting peckish.
+Dog is getting sleepy.
+Dog is extremely happy!
+Dog is doing okay but could use some attention.
